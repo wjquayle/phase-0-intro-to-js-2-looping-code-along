b8ba61c231687ef6b863aa606cdb83cac93d4656
Bill <wjquayle@gmail.com>
2023-07-07 23:18:34 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards(names,event) {
+  const message = [];
+
+  for (let i = 0; i < names.length; i++) {
+    message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+
+    debugger;
+  }
+
+  return message;
+}
+
+function countDown(count) {
+  while(count!== -1) {
+    console.log(count)
+    count--;
+  }
+  return count;
+}
